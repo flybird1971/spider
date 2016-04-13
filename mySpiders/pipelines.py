56013e78d85ebd87930761539820bffa8eb82e39
flybird1971 <flybird1971@gmail.com>
2016-04-13 20:27:59 +0800

    修改pipline

--- mySpiders/pipelines.py
+++ mySpiders/pipelines.py
@@ -50,6 +50,7 @@ class XmlFeedPipeline(object):
         database = db_name
         self.db = Mysql(config, database)
         self.tableName = db_table_name
+        self.item = None
 
     def process_item(self, item, spider):
 
@@ -57,16 +58,59 @@ class XmlFeedPipeline(object):
             logging.info('-----------------------list page repeat ')
             return True
 
+        self.item = item
+        insertDataList = self.filterAndPackageDgrate()
+        for index in insertDataList:
+            self.db.insert(self.tableName, insertDataList[index])
+
+        # rule_id = item['rule_id']
+        # public_time = int(time.time())
+        # create_time = int(time.time())
+        # for index, title in enumerate(item['title']):
+
+        #     uniqueCode = toMd5(item['source_url'][index])
+        #     if not requstDistinct(uniqueCode):
+        #         logging.info("-----------%s----has exists----------" % item['source_url'][index])
+        #         continue
+
+        #     if index < len(item['img_url']) and item['img_url'][index]:
+        #         img_url = json.dumps(item['img_url'][index])
+        #     else:
+        #         img_url = ''
+
+        #     if index < len(item['description']) and item['description'][index]:
+        #         description = item['description'][index]
+        #     else:
+        #         continue
+
+        #     title = title.decode('utf8')[0:255].encode('utf8')
+
+        #     insertData = {
+        #         'source_url': item['source_url'][index],
+        #         'unique_code': uniqueCode,
+        #         'rule_id': rule_id,
+        #         'title': title,
+        #         'description': description,
+        #         'img_url': img_url,
+        #         'public_time': public_time,
+        #         'create_time': create_time
+        #     }
+        #    self.db.insert(self.tableName, insertData)
+        return True
+
+    def filterAndPackageDgrate(self):
+
+        uniqueCodeList = []
+        insertData = {}
+        item = self.item
+
         rule_id = item['rule_id']
         public_time = int(time.time())
         create_time = int(time.time())
+
         for index, title in enumerate(item['title']):
 
             uniqueCode = toMd5(item['source_url'][index])
-            if not requstDistinct(uniqueCode):
-                logging.info("-----------%s----has exists----------" % item['source_url'][index])
-                continue
-
             if index < len(item['img_url']) and item['img_url'][index]:
                 img_url = json.dumps(item['img_url'][index])
             else:
@@ -78,8 +122,8 @@ class XmlFeedPipeline(object):
                 continue
 
             title = title.decode('utf8')[0:255].encode('utf8')
-
-            insertData = {
+            uniqueCodeList.append(uniqueCode)
+            insertData[uniqueCode] = {
                 'source_url': item['source_url'][index],
                 'unique_code': uniqueCode,
                 'rule_id': rule_id,
@@ -89,5 +133,11 @@ class XmlFeedPipeline(object):
                 'public_time': public_time,
                 'create_time': create_time
             }
-            self.db.insert(self.tableName, insertData)
-        return True
+            # self.db.insert(self.tableName, insertData)
+
+        if uniqueCodeList:
+            repeatUniqueCode = requstDistinct(uniqueCodeList)
+            for i, unique in enumerate(repeatUniqueCode):
+                del(insertData[unique])
+
+        return insertData
