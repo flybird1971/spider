9a8255542669afa50cdeaac6d6a95650fe14ec64
flybird1971 <flybird1971@gmail.com>
2016-04-13 18:00:51 +0800

    扩展hash能力，可以hash list dict tuple set

--- mySpiders/utils/hash.py
+++ mySpiders/utils/hash.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
-#coding:utf-8
+# coding:utf-8
 
 import md5
 
-def toMd5(data):
-        m = md5.new()
-        m.update(data);
-        return m.hexdigest()
 
-__all__=['toMd5']
+def toMd5(data):
+
+    if isinstance(data, (list, dict, tuple, set)):
+        string = " ".join(data)
+    else:
+        string = data
+    string = string.encode('utf-8')
+    m = md5.new()
+    m.update(string)
+    return m.hexdigest()
+
+__all__ = ['toMd5']
