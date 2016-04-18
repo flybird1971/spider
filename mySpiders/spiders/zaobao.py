bada4f2c91e65d3431786826edbc4e5830958ff0
flybird1971 <flybird1971@gmail.com>
2016-04-18 18:48:24 +0800

    除去冗余代码

--- mySpiders/spiders/zaobao.py
+++ mySpiders/spiders/zaobao.py
@@ -3,29 +3,29 @@ import scrapy
 
 
 class ZaobaoSpider(scrapy.Spider):
-    #爬虫名称，工程中要求唯一
+    # 爬虫名称，工程中要求唯一
     name = "zaobaos"
 
-    #允许的域名
+    # 允许的域名
     allowed_domains = ["zaobao.com"]
 
-    #初始待抓取url列表
+    # 初始待抓取url列表
     start_urls = (
         'http://www.zaobao.com/',
     )
 
-    #用户设置，会覆盖全局设置
+    # 用户设置，会覆盖全局设置
     custom_settings = (
 
     )
 
-    #抓取器，spider将绑定到它上面
+    # 抓取器，spider将绑定到它上面
     crawler = {}
 
-    #配置实例，包含工程中所有配置变量
+    # 配置实例，包含工程中所有配置变量
     settings = {}
 
-    #日志实例
+    # 日志实例
     logger = {}
 
     def parse(self, response):
